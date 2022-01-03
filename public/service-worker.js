d05bea94f727533ce8e637a3bddcf03def5d5393
Nadine <nadine_paez@hotmail.com>
2022-01-03 11:37:27 -0500

    fixed service-worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -49,17 +49,11 @@ self.addEventListener('activate', function(e) {
     console.log('fetch request : ' + e.request.url)
     e.respondWith(
         //source already in cache
-        caches.match(e.request).then(function (request) {
-            if (request) {
-              console.log('responding with cache : ' + e.request.url)
-              return request
-            } 
-          }),
-          //if resource is not in cache, retrieve it
           caches.match(e.request).then(function (request) {
             if (request) {
               console.log('responding with cache : ' + e.request.url)
               return request
+              //if file is not found in cache, fetch
             } else {
               console.log('file is not cached, fetching : ' + e.request.url)
               return fetch(e.request)
